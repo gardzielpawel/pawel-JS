eec7cf6b2569e80f588b17ad92a6b5775513d119
Paweł Gardziel <gardzielpawel@gmail.com>
2018-01-15 10:59:43 +0100

    moduł 8.6 instrukcje warunkowe

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,7 +11,19 @@ console.log('Hello, ' + name);
 	hwdbfogbwldfugwiueghowiug
 */
 
-var a = 3;
+var f = 3;
 var h = 8;
-var triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var triangleArea = f*h/2;
+console.log('Triangle field with base f: ' + f + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+var a = 4;
+var b = 9;
+var equal = (a * a) - (2 * a * b) + (b * b);
+
+if  (equal < 0 ) {
+	console.log('Wynik ujemny');
+} else if (equal > 0 ) {
+	console.log('Wynik dodatni');
+} else {
+	console.log('Zero');
+}  
